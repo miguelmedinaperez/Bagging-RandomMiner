10eede556b679d50d07c7ce1c9a35b870c878f48
Prof. Miguel Angel Medina-Pérez, PhD <47543798+miguelmedinaperez@users.noreply.github.com>
2020-03-22 20:25:43 -0600

    Add files via upload

---
+++ BaggingRandomMiner.cs
@@ -0,0 +1,127 @@
+﻿/*
+ * Created by: Miguel Angel Medina Pérez (miguelmedinaperez@gmail.com)
+ * Created: 11/16/2018
+ * Comments by: Miguel Angel Medina Pérez (miguelmedinaperez@gmail.com)
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using PRFramework.Core.Clustering;
+using PRFramework.Core.Common;
+using PRFramework.Core.ComparisonFunctions;
+using PRFramework.Core.Samplers;
+using PRFramework.Core.SupervisedClassifiers;
+using PRFramework.Core.Vector;
+
+namespace PRFramework.MigueExperimenter.AnomalyDetection
+{
+    public class BaggingRandomMiner : ISupervisedClassifier
+    {
+        public int ClassifierCount { set; get; } = 100;
+
+        public int BootstrapSamplePercent { set; get; } = 100;
+
+        public bool UseBootstrapSampleCount { set; get; } = false;
+
+        public int BootstrapSampleCount { set; get; } = 0;
+
+        public bool UsePastEvenQueue { set; get; } = false;
+
+        public double[] Classify(Instance instance)
+        {
+            var instanceVector = new InstanceVector(instance);
+            double currentSimilarity = 0;
+            for (int i = 0; i < ClassifierCount; i++)
+            {
+                double minDistance = double.MaxValue;
+                foreach (var center in _centers[i])
+                {
+                    double d = _distance.Compare(instanceVector, center);
+                    if (d < minDistance)
+                        minDistance = d;
+                }
+
+                currentSimilarity += minDistance > 0 ? Math.Exp(-(minDistance * minDistance) / (2 * _sd[i] * _sd[i])) : 1;
+            }
+            currentSimilarity /= ClassifierCount;
+            if (currentSimilarity < 0)
+                currentSimilarity = 0;
+
+            if (!UsePastEvenQueue)
+                return new double[1] { 1-currentSimilarity };
+
+            double resultSimilarity = (_alpha * _similaritySum / _maxEventCount + (1 - _alpha) * currentSimilarity);
+            if (resultSimilarity < 0)
+                resultSimilarity = 0;
+
+            _similaritySum += currentSimilarity;
+            if (_pastEvents.Count == _maxEventCount)
+                _similaritySum -= _pastEvents.Dequeue();
+
+            _pastEvents.Enqueue(currentSimilarity);
+
+            if (_similaritySum < 0)
+                _similaritySum = 0;
+
+            return new double[1] { 1-resultSimilarity }; 
+        }
+
+        public void Train(InstanceModel model, IEnumerable<Instance> dataset)
+        {
+            var classFeature = model.ClassFeature() as NominalFeature;
+            List<Feature> featuresToConsider = new List<Feature>();
+            for (int i = 0; i < model.Features.Length; i++)
+                if (model.Features[i] != classFeature)
+                {
+                    featuresToConsider.Add(model.Features[i]);
+                }
+
+            _distance = new EuclideanDissimilarity(dataset.ToList(), model);
+
+
+            var instanceList = dataset.ToList();
+            var randomSampler = new RandomSamplerWithReplacement<Instance>();
+            _sd = new double[ClassifierCount];
+            _centers = new IEnumerable<Instance>[ClassifierCount];
+
+            for (int i = 0; i < ClassifierCount; i++)
+            {
+                int sampleSize = UseBootstrapSampleCount
+                    ? BootstrapSampleCount : (BootstrapSamplePercent * instanceList.Count / 100);
+
+                _centers[i] = randomSampler.GetSample(dataset, sampleSize);
+
+                _sd[i] = ComputeBeta(_centers[i].ToList(), _distance);
+            }
+        }
+
+        private double ComputeBeta(List<Instance> prObjectVectorsList, IDissimilarityFunction<Instance> distance)
+        {
+            double sum = 0;
+            int count = 0;
+            for (int i = 0; i < prObjectVectorsList.Count - 1; i++)
+                for (int j = i + 1; j < prObjectVectorsList.Count; j++)
+                {
+                    sum += distance.Compare(prObjectVectorsList[i], prObjectVectorsList[j]);
+                    count++;
+                }
+
+            return sum / count;
+        }
+
+        private EuclideanDissimilarity _distance;
+
+        private double[] _sd = null;
+
+        private IEnumerable<Instance>[] _centers = null;
+
+        private Queue<double> _pastEvents = new Queue<double>();
+
+        private double _similaritySum = 0;
+
+        private const int _maxEventCount = 3;
+
+        private const double _alpha = 0.5;
+    }
+}
---
+++ EuclideanDissimilarity.cs
@@ -0,0 +1,231 @@
+﻿/*
+ * Created by: Miguel Angel Medina Pérez (miguelmedinaperez@gmail.com)
+ * Created: 05/18/2017
+ * Comments by: Miguel Angel Medina Pérez (miguelmedinaperez@gmail.com)
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using PRFramework.Core.Common;
+using PRFramework.Core.SupervisedClassifiers;
+
+namespace PRFramework.Core.ComparisonFunctions
+{
+    /// <summary>
+    ///     A generic implementation of Euclidean distance with normalization. 
+    /// </summary>
+    /// <remarks>
+    ///     <para>
+    ///         The constructor of the class requires a delegate <see cref="GetComponent{IVector}"/> to provide the values of the components of the vectors.
+    ///     </para>
+    /// </remarks>
+    /// <typeparam name="IVector">A type of vector.</typeparam>
+    [Serializable]
+    public class EuclideanDissimilarity : IDissimilarityFunction<Instance>
+    {
+        public EuclideanDissimilarity(IEnumerable<Instance> instances, InstanceModel model)
+        {
+            _instanceModel = model;
+            var classFeature = model.ClassFeature();
+            _features = _instanceModel.Features.Where(f => f != classFeature).ToList();
+
+            int instanceCount = 0;
+
+            foreach (var instance in instances)
+            {
+                if (_instanceModel != instance.Model)
+                    throw new ArgumentOutOfRangeException(nameof(model), model, $"Unable to instantiate ${nameof(EuclideanDissimilarity)}: Object found with invalid ${nameof(InstanceModel)}.");
+
+                if (_minFeatureValues != null)
+                {
+                    foreach (var feature in _features)
+                        if (!Double.IsNaN(instance[feature]))
+                        {
+                            if (feature.FeatureType == FeatureType.Double || feature.FeatureType == FeatureType.Integer)
+                            {
+                                if (Double.IsNaN(_minFeatureValues[feature.Index]) || instance[feature] < _minFeatureValues[feature.Index])
+                                    _minFeatureValues[feature.Index] = instance[feature];
+
+                                if (Double.IsNaN(_maxFeatureValues[feature.Index]) || instance[feature] > _maxFeatureValues[feature.Index])
+                                    _maxFeatureValues[feature.Index] = instance[feature];
+                            }
+                        }
+                }
+                else
+                {
+                    _minFeatureValues = new double[_features.Count];
+                    _maxFeatureValues = new double[_features.Count];
+                    foreach (var feature in _features)
+                        if (Double.IsNaN(instance[feature]))
+                        {
+                            _minFeatureValues[feature.Index] = Double.NaN;
+                            _maxFeatureValues[feature.Index] = Double.NaN;
+                        }
+                        else
+                        {
+                            if (feature.FeatureType == FeatureType.Double || feature.FeatureType == FeatureType.Integer)
+                            {
+                                _minFeatureValues[feature.Index] = instance[feature];
+                                _maxFeatureValues[feature.Index] = instance[feature];
+                            }
+                            else if (feature.FeatureType == FeatureType.Nominal)
+                            {
+                                _minFeatureValues[feature.Index] = Double.NaN;
+                                _maxFeatureValues[feature.Index] = Double.NaN;
+                            }
+                        }
+                }
+
+                instanceCount++;
+            }
+
+            if (instanceCount < 1)
+                throw new ArgumentOutOfRangeException(nameof(instances), instances, $"Unable to instantiate ${nameof(EuclideanDissimilarity)}: empty vector collection.");
+
+            _maxLessMin = new double[_minFeatureValues.Length];
+            double validFeaturesCount = 0;
+            for (int i = 0; i < _minFeatureValues.Length; i++)
+            {
+                _maxLessMin[i] = _maxFeatureValues[i] - _minFeatureValues[i];
+                if (!Double.IsNaN(_maxLessMin[i]) || (_features[i].FeatureType == FeatureType.Nominal))
+                    validFeaturesCount++;
+            }
+
+            _maxDissimilarity = Math.Sqrt(validFeaturesCount);
+        }
+
+        public double Compare(Instance source, Instance compareTo)
+        {
+            try
+            {
+                double sum = 0;
+                foreach (var feature in _features)
+                    if (!Double.IsNaN(source[feature]) && !Double.IsNaN(compareTo[feature]))
+                    {
+                        if (feature.FeatureType == FeatureType.Double || feature.FeatureType == FeatureType.Integer)
+                        {
+                            if (!double.IsNaN(_maxLessMin[feature.Index]) && _maxLessMin[feature.Index] > 0)
+                            {
+                                double componentDiff = Math.Abs(source[feature] - compareTo[feature]) / _maxLessMin[feature.Index];
+                                sum += componentDiff > 1 ? 1 : Math.Pow(componentDiff, 2);
+                            }
+                        }
+                        else if (feature.FeatureType == FeatureType.Nominal &&
+                                 (int)source[feature] != (int)compareTo[feature])
+                            sum += 1;
+                    }
+                    else
+                        sum += 1;
+
+                return Math.Sqrt(sum) / _maxDissimilarity;
+            }
+            catch (Exception e)
+            {
+                if (_instanceModel.Features.Length != source.Model.Features.Length)
+                    throw new ArgumentOutOfRangeException(nameof(source), source,
+                        "Unable to compare objects: Invalid instance model");
+                if (_instanceModel.Features.Length != compareTo.Model.Features.Length)
+                    throw new ArgumentOutOfRangeException(nameof(compareTo), compareTo,
+                        "Unable to compare objects: Invalid instance model");
+                for (int i = 0; i < source.Model.Features.Length; i++)
+                {
+                    if (source.Model.Features[i].FeatureType != _instanceModel.Features[i].FeatureType)
+                        throw new ArgumentOutOfRangeException(nameof(source), source,
+                            "Unable to compare objects: Invalid instance model");
+                    if (compareTo.Model.Features[i].FeatureType != _instanceModel.Features[i].FeatureType)
+                        throw new ArgumentOutOfRangeException(nameof(compareTo), compareTo,
+                            "Unable to compare objects: Invalid instance model");
+                }
+
+                throw;
+            }
+        }
+
+        public double Compare(Instance source, Instance compareTo, IEnumerable<Feature> featureSubset)
+        {
+            try
+            {
+                double sum = 0;
+                double validFeaturesCount = 0;
+                foreach (var feature in featureSubset)
+                {
+                    if (!Double.IsNaN(source[feature]) && !Double.IsNaN(compareTo[feature]))
+                    {
+                        if (feature.FeatureType == FeatureType.Double || feature.FeatureType == FeatureType.Integer)
+                        {
+                            if (!double.IsNaN(_maxLessMin[feature.Index]) && _maxLessMin[feature.Index] > 0)
+                            {
+                                double componentDiff = Math.Abs(source[feature] - compareTo[feature]) / _maxLessMin[feature.Index];
+                                sum += componentDiff > 1 ? 1 : Math.Pow(componentDiff, 2);
+                            }
+                        }
+                        else if (feature.FeatureType == FeatureType.Nominal &&
+                                 (int)source[feature] != (int)compareTo[feature])
+                            sum += 1;
+                    }
+                    else
+                        sum += 1;
+
+                    validFeaturesCount++;
+                }
+
+                return Math.Sqrt(sum) / Math.Sqrt(validFeaturesCount);
+            }
+            catch (Exception e)
+            {
+                if (_instanceModel.Features.Length != source.Model.Features.Length)
+                    throw new ArgumentOutOfRangeException(nameof(source), source,
+                        "Unable to compare objects: Invalid instance model");
+                if (_instanceModel.Features.Length != compareTo.Model.Features.Length)
+                    throw new ArgumentOutOfRangeException(nameof(compareTo), compareTo,
+                        "Unable to compare objects: Invalid instance model");
+                for (int i = 0; i < source.Model.Features.Length; i++)
+                {
+                    if (source.Model.Features[i].FeatureType != _instanceModel.Features[i].FeatureType)
+                        throw new ArgumentOutOfRangeException(nameof(source), source,
+                            "Unable to compare objects: Invalid instance model");
+                    if (compareTo.Model.Features[i].FeatureType != _instanceModel.Features[i].FeatureType)
+                        throw new ArgumentOutOfRangeException(nameof(compareTo), compareTo,
+                            "Unable to compare objects: Invalid instance model");
+                }
+
+                throw;
+            }
+        }
+
+        public void UpdateTraining(Instance instance)
+        {
+            foreach (var feature in _features)
+                if (!Double.IsNaN(instance[feature]))
+                {
+                    if (feature.FeatureType == FeatureType.Double || feature.FeatureType == FeatureType.Integer)
+                    {
+                        if (Double.IsNaN(_minFeatureValues[feature.Index]) || instance[feature] < _minFeatureValues[feature.Index])
+                            _minFeatureValues[feature.Index] = instance[feature];
+
+                        if (Double.IsNaN(_maxFeatureValues[feature.Index]) || instance[feature] > _maxFeatureValues[feature.Index])
+                            _maxFeatureValues[feature.Index] = instance[feature];
+                    }
+                }
+
+            _maxLessMin = new double[_minFeatureValues.Length];
+            double validFeaturesCount = 0;
+            for (int i = 0; i < _minFeatureValues.Length; i++)
+            {
+                _maxLessMin[i] = _maxFeatureValues[i] - _minFeatureValues[i];
+                if (!Double.IsNaN(_maxLessMin[i]) || (_features[i].FeatureType == FeatureType.Nominal))
+                    validFeaturesCount++;
+            }
+
+            _maxDissimilarity = Math.Sqrt(validFeaturesCount);
+        }
+
+        private double[] _minFeatureValues = null;
+        private double[] _maxFeatureValues = null;
+        private double[] _maxLessMin;
+        private double _maxDissimilarity;
+        private readonly List<Feature> _features;
+        private readonly InstanceModel _instanceModel;
+    }
+}
